4301cb76929fdcc1bf76e97983ccc0f0d58aad8d
Chris M <webmaster@magnaws.com>
2018-05-22 15:48:20 -0400

    Just tweaked the comments.

--- config/s_config.class.php
+++ config/s_config.class.php
@@ -23,6 +23,10 @@ require_once(dirname(dirname(dirname(__FILE__))).'/t_config.interface.php');    
 
 /**
 This class is a static class with a global scope. It contains the various configuartion settings for the Rift Valley Platform installation.
+
+We have two separate database, with the main data database holding the actual data, and the security database, holding logins and security tokens.
+
+These databases do not need to have any relations, may be totally different technologies (we are currently limited to MySQL and Postgres), and can even be on different hosts.
  */
 class CO_Config {
     static private $_god_mode_id = 2;   ///< God Login Security DB ID. This is private, so it can't be programmatically changed.
@@ -32,17 +36,16 @@ class CO_Config {
     static $min_pw_len = 8;             ///< The minimum password length.
     
     static $data_db_name = <MAIN DB NAME>;                          ///< The PDO name for the main data database.
-    static $data_db_host = <MAIN DB HOST (USUALLY 'localhost')>;    ///< The URL name for the Main data database host.
+    static $data_db_host = <MAIN DB HOST (USUALLY 'localhost')>;    ///< The URL name for the main data database host.
     static $data_db_type = <'pgsql' OR 'mysql'>;                    ///< The technology used for the main data database (MySQL or Postgres).
     static $data_db_login = <MAIN DB LOGIN>;                        ///< The string login name for the main data database.
     static $data_db_password = <MAIN DB PASSWORD>;                  ///< The main data database password string.
 
-    /// These are the same as above, but for the security database.
-    static $sec_db_name = <SECURITY DB NAME>;
-    static $sec_db_host = <SECURITY DB HOST (USUALLY 'localhost')>;
-    static $sec_db_type = <'pgsql' OR 'mysql'>;
-    static $sec_db_login = <SECURITY DB LOGIN>;
-    static $sec_db_password = <SECURITY DB PASSWORD>;
+    static $sec_db_name = <SECURITY DB NAME>;                       ///< The PDO name for the security database.
+    static $sec_db_host = <SECURITY DB HOST (USUALLY 'localhost')>; ///< The URL name for the security database host.
+    static $sec_db_type = <'pgsql' OR 'mysql'>;                     ///< The technology used for the security database (MySQL or Postgres).
+    static $sec_db_login = <SECURITY DB LOGIN>;                     ///< The string login name for the security database.
+    static $sec_db_password = <SECURITY DB PASSWORD>;               ///< The security database password string.
 
     /**
     This is the Google API key. It's required for CHAMELEON to do address lookups and other geocoding tasks.
@@ -53,6 +56,8 @@ class CO_Config {
     /***********************/
     /**
     \returns the POSIX path to the main ANDISOL directory.
+    
+    You can change this to reflect a different directory path if you want. It should be absolute POSIX.
      */
     static function base_dir() {
         return dirname(dirname(dirname(__FILE__)));
