a9e7fcdb2fa204e13631d163d58dad16fd79d966
Chris Marshall <chris@riftvalleysoftware.com>
2021-02-10 08:54:22 -0500

    Updated the SQL to include the personal_ids table column in the security SQL.

--- test/sql/search_tests_security_pgsql.sql
+++ test/sql/search_tests_security_pgsql.sql
@@ -11,15 +11,16 @@ CREATE TABLE co_security_nodes (
   write_security_id BIGINT DEFAULT NULL,
   object_name VARCHAR(255) DEFAULT NULL,
   access_class_context VARCHAR(4095) DEFAULT NULL,
+  ids VARCHAR(4095) DEFAULT NULL,
   ids VARCHAR(4095) DEFAULT NULL
 );
 
-INSERT INTO co_security_nodes (api_key, login_id, access_class, last_access, read_security_id, write_security_id, object_name, access_class_context, ids) VALUES
-(NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL),
-(NULL, 'admin', 'CO_Security_Login', '1970-01-01 00:00:00', 1, 2, 'Default Admin', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL),
-(NULL, 'MDAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 3, 'Maryland Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'VAAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 4, 'Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'DCAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 5, 'Washington DC Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'WVAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 6, 'West Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'DEAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 7, 'Delaware Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', ''),
-(NULL, 'DCAreaManager', 'CO_Login_Manager', '1970-01-01 00:00:00', 1, 8, 'Manages All Logins', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '2,3,4,5,6,7');
+INSERT INTO co_security_nodes (api_key, login_id, access_class, last_access, read_security_id, write_security_id, object_name, access_class_context, ids, personal_ids) VALUES
+(NULL, 'CO_Security_Node', '1970-01-01 00:00:00', -1, -1, NULL, NULL, NULL, NULL),
+(NULL, 'admin', 'CO_Security_Login', '1970-01-01 00:00:00', 1, 2, 'Default Admin', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL, NULL),
+(NULL, 'MDAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 3, 'Maryland Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'VAAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 4, 'Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'DCAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 5, 'Washington DC Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'WVAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 6, 'West Virginia Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'DEAdmin', 'CO_Cobra_Login', '1970-01-01 00:00:00', 1, 7, 'Delaware Login', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '', NULL),
+(NULL, 'DCAreaManager', 'CO_Login_Manager', '1970-01-01 00:00:00', 1, 8, 'Manages All Logins', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:13:\"CodYOzPtwxb4A\";}', '2,3,4,5,6,7', NULL);
